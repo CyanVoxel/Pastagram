56a61e412e904f06f3c40e16e307ecee9f79b49a
TRAVIS ABENDSHIEN <8000602355@wce111-15.local>
2019-02-28 19:58:05 -0800

    Initial Commit

---
+++ Pastagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pastagram
+//
+//  Created by TRAVIS ABENDSHIEN on 2/28/19.
+//  Copyright © 2019 TRAVIS ABENDSHIEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
