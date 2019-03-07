6cfb7c9882c8fe6780724af24f705e2f81ebb2fe
Travis Abendshien <cyanvoxel@Traviss-Mac.local>
2019-03-07 09:33:44 -0800

    -Users can now sign up and log in -Added Image assets

---
+++ Pastagram/CameraViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CameraViewController.swift
+//  Pastagram
+//
+//  Created by Travis Abendshien on 3/7/19.
+//  Copyright © 2019 TRAVIS ABENDSHIEN. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        
+    } //onSubmit()
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+        
+    } //onCameraButton()
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
